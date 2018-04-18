ba8addd4bb06ac11050737fabfaf68edbd45ab1f
Rachel Tung <rachtung12@gmail.com>
2018-04-18 13:40:53 +0800

    Initial project

---
+++ roma/src/main/java/com/example/roma/RomaApplication.java
@@ -0,0 +1,12 @@
+package com.example.roma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomaApplication.class, args);
+	}
+}
